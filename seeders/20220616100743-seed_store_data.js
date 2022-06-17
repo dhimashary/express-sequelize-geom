acc24664c527b0d4e834bb7fdb4e9e34b4496da4
dhimashary <dhimas.hary@gmail.com>
2022-06-17 10:17:19 +0700

    init express & db

---
+++ seeders/20220616100743-seed_store_data.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Stores', [
+      {
+        name: 'Bus Leuwipinjang',
+        location: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(107.59278847659893 -6.942981263106864)'
+        ),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Durian Sultan',
+        location: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(107.5904275402039 -6.9439994342171225)'
+        ),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Kantin Siantar',
+        location: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(107.5925576773082 -6.940669415817259)'
+        ),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'CNI Bandung',
+        location: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(107.59422277037818 -6.937911900280693)'
+        ),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Store', null, {});
+  },
+};
